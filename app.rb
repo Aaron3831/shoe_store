57e9e9100480eb1c7f4c539534b8141ec0ec7d56
Aaron Nguyen <aaron3831@gmail.com>
2017-05-12 08:52:16 -0700

    add routing to append shoe name

--- app.rb
+++ app.rb
@@ -7,3 +7,14 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get ('/shoes') do
+  @shoes = Shoe.all()
+  erb(:shoes)
+end
+
+post ('/shoes') do
+  shoe_name = params.fetch('shoe_name')
+  @shoes = Shoe.create({:brand => shoe_name})
+  redirect("/shoes")
+end
